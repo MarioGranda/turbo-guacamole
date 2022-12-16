0939bb115d7ec5b5fbb9bc6728a911bcd3b01c6f
Mario Granda <mario-granda@hotmail.com>
2022-12-16 19:21:01 +0100

    Add navigation provider

--- App.tsx
+++ App.tsx
@@ -1,15 +1,18 @@
 import { Provider } from "react-redux"
 import { store } from './redux/store';
 import HomeScreen from './screens/HomeScreen';
-import {SafeAreaProvider} from "react-native-safe-area-context"
+import { SafeAreaProvider } from "react-native-safe-area-context"
+import { NavigationContainer } from '@react-navigation/native';
 
 
 export default function App() {
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-      <HomeScreen />
-      </SafeAreaProvider>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <HomeScreen />
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
