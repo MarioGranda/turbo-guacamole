6ed900c90c9175ba8874bea163a6810a194a289a
Mario Granda <mario-granda@hotmail.com>
2022-12-19 11:15:44 +0100

    Add MapScreen

---
+++ screens/MapScreen.tsx
@@ -0,0 +1,12 @@
+import { Image, SafeAreaView, View } from "react-native";
+import tw from 'twrnc';
+import NavOptions from "./NavOptions";
+
+export default function MapScreen() {
+  return (
+    <SafeAreaView style={tw`bg-red-900 h-full`}>
+      <View style={tw`text-red-400 p-5`}>
+      </View>
+    </SafeAreaView>
+  );
+}
