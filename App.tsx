dfc95a92e1711f5bdbe5acbf5254d7c3d8a11611
Mario Granda <mario-granda@hotmail.com>
2022-12-20 14:00:28 +0100

    Add keyboard avoiding view

--- App.tsx
+++ App.tsx
@@ -5,6 +5,7 @@ import { SafeAreaProvider } from "react-native-safe-area-context"
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from "@react-navigation/stack"
 import MapScreen from "./screens/MapScreen";
+import { KeyboardAvoidingView, Platform } from "react-native";
 
 const Stack = createStackNavigator()
 
@@ -13,16 +14,19 @@ export default function App() {
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen name='HomeScreen' component={HomeScreen} 
-            options={{
-              headerShown: false
-            }}/>
-             <Stack.Screen name='MapScreen' component={MapScreen} 
-            options={{
-              headerShown: false
-            }}/>
-          </Stack.Navigator>
+          <KeyboardAvoidingView style={{ flex: 1 }}
+          behavior={Platform.OS === "ios" ? "padding" : "height"}>
+            <Stack.Navigator>
+              <Stack.Screen name='HomeScreen' component={HomeScreen}
+                options={{
+                  headerShown: false
+                }} />
+              <Stack.Screen name='MapScreen' component={MapScreen}
+                options={{
+                  headerShown: false
+                }} />
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
